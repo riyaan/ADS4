32ae507fb3dae3ceda2efe938500475902c62492
Riyaan Williams <riyaan.williams@gmail.com>
2013-09-19 16:02:54 +0200

    Got the regular expression sorted.

---
+++ Application Layer/Tests/Components/MovementControl.Tests/ParserUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovementControl.Tests
+{
+    [TestClass]
+    public class ParserUnitTests
+    {
+        [TestMethod]
+        public void ParseSuccess()
+        {
+            Parser parser = new Parser();
+            ExpressionBase eb = parser.Parse("F2");
+
+            Assert.AreNotEqual(null, eb);
+        }
+    }
+}
