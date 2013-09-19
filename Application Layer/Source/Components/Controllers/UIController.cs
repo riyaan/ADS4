ceb508861178aa68eae3cd29ba323092250cf694
Riyaan Williams <riyaan.williams@gmail.com>
2013-09-19 13:07:28 +0200

    Created the maze graphically using Buttons.

---
+++ Application Layer/Source/Components/Controllers/UIController.cs
@@ -0,0 +1,23 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class UIController
+    {
+        public UIController()
+        {
+        }
+
+        public Maze GenerateNewMaze(int rows, int columns)
+        {
+            // UI Controller interacts with the Maze Controller
+            MazeController mazeController = new MazeController(rows, columns);
+            return mazeController.Maze;
+        }
+    }
+}
--- Presentation Layer/WindowsFormsApplication1/Navigator.cs
+++ Presentation Layer/WindowsFormsApplication1/Navigator.cs
@@ -1,6 +1,7 @@
 ﻿using Controllers;
 using Entities;
 using System;
+using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
@@ -39,24 +40,15 @@ namespace MazeNavigatorUI
             set { maze = value; }
         }
 
-        private StringBuilder stringBuilder;
-        public StringBuilder StringBuilder
-        {
-            get { return stringBuilder; }
-            set { stringBuilder = value; }
-        }
-
         public NavigatorUI()
         {
             InitializeComponent();            
 
-            Random = new Random();
-            StringBuilder = new StringBuilder();
+            //Random = new Random((int)DateTime.Now.Ticks);
+            //Rows = Random.Next(3, MAX_ROWS);
+            //Columns = Random.Next(3, MAX_COLUMNS);
 
-            Rows = Random.Next(3, MAX_ROWS);
-            Columns = Random.Next(3, MAX_COLUMNS);
-
-            this.ClientSize = new System.Drawing.Size(rows*100, columns*100);
+            //this.ClientSize = new System.Drawing.Size(rows*100, columns*100);
             flowLayoutPanel1.Size = new System.Drawing.Size(this.Height-10, this.Width-10);            
         }
 
@@ -66,16 +58,46 @@ namespace MazeNavigatorUI
 
         private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            flowLayoutPanel1.Controls.Clear();
+
             // UI Controller interacts with the Maze Controller
-            MazeController mazeController = new MazeController(MAX_ROWS, MAX_COLUMNS);
-            Maze = mazeController.Maze;
+            UIController uiController = new UIController();
+            Maze = uiController.GenerateNewMaze(MAX_ROWS, MAX_COLUMNS);
 
-
+            CreateMazeVisually();
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void CreateMazeVisually()
+        {
+            //string output = String.Empty;
+            for (int i = Maze.Rows - 1; i >= 0; i--)
+            {
+                for (int j = Maze.Columns - 1; j >= 0; j--)
+                {
+                    Button b = new Button
+                    {
+                        Name = Maze.Grid[i, j].XCoordinate + "_" + Maze.Grid[i, j].YCoordinate
+                    };
+
+                    if (Maze.Grid[i, j].CellState == CELL_STATE.OPEN)
+                    {
+                        b.BackColor = Color.Blue;
+                        //output += "|O|";
+                    }
+                    else
+                    {                        
+                        //output += "|X|";
+                    }
+
+                    flowLayoutPanel1.Controls.Add(b);
+                }
+                //output += System.Environment.NewLine;
+            }
         }
     }
 }
